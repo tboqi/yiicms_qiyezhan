47ac5459f2f4c3d0be6e0201869df0eaf7191e5a
tbq <tbq2616@sina.com>
2019-10-25 13:33:35 +0800

    my zhanshen

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=10.0.75.1;dbname=yiicms_qiyezhan',
+    'dsn' => 'mysql:host=192.168.99.1;dbname=yiicms_qiyezhan',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
